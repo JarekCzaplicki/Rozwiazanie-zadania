b693446a810462b63c6cc15337f900f61ddda1a6
jarekczaplicki <jarekczaplicki@tlen.pl>
2024-06-10 17:39:25 +0200

    Initial commit

---
+++ src/test/java/com/rozwiazaniezadania/RozwiazanieZadaniaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rozwiazaniezadania;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RozwiazanieZadaniaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
